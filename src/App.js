408e3652a8c5e1e3a861f5ef96a5c308a4eba7d0
YuhangXiong <yxiong4@ualberta.ca>
2021-12-03 14:18:08 -0700

    added the search function css and make the searchbox component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     }
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -17,10 +21,24 @@ class App extends Component {
       .then(monsters => this.setState({ monsters: monsters }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    console.log(searchField)
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        {/* <input type='search' placeholder='search monsters' onChange={e => this.setState({ searchField: e.target.value })} /> */}
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange} />
+        {/* <SearchBox placeholder='search monsters' handlerChange={this.onSearchChange} /> */}
+
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
