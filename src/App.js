be65c2c26e16e9d01894c7773674e134f2915242
YuhangXiong <yxiong4@ualberta.ca>
2021-12-02 22:48:08 -0700

    added card list component and css grid to show the json content fetched fromthe api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import './App.css';
 import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Franklin'
-        },
-        {
-          name: 'Cheers'
-        },
-        {
-          name: 'Bluekey'
-        },
-      ]
+      monsters: []
     }
   }
 
-
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(monsters => this.setState({ monsters: monsters }));
+  }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => <h1>{monster.name}</h1>)}
+        <CardList name='yuhang' >
+          {this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)}
+        </CardList>
 
       </div>
     );
